3cb8465ef6a84061c5ce225114a5ec1feec16cd4
rezazulf <rezazulfff@gmail.com>
2022-10-11 02:51:00 +0700

    add dropdown menu for map selection

--- resources/views/depan.blade.php
+++ resources/views/depan.blade.php
@@ -29,7 +29,7 @@
 
 <body>
    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Smart Trash</a>
+      <a class="navbar-brand" href="/">Smart Trash</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
